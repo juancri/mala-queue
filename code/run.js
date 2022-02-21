e72d21a9905b91bf7dd56f84f7e5569645e36419
JC Olivares <juancri@juancri.com>
2022-02-21 16:00:34 -0600

    Creando funciones para UI Verificando sesión

--- code/run.js
+++ code/run.js
@@ -1,63 +1,96 @@
 
 (async function()
 {
-  // Constantes
-  const JC_EVENTO = 'biz175';
-  const JC_NUM_FECHA = 1;
-  const JC_VERSION = 1;
-
-  // Obtener ubicaciones disponibles
-  const response1 = await fetch("/Compra/TraerTipoTicketsSectores", {
-    headers: { "content-type": "application/json" },
-    body: JSON.stringify({ eventoID: JC_EVENTO, eventoCalendarioID: JC_NUM_FECHA }),
-    method: "POST",
-    credentials: "include"
-  });
-  const body1 = JSON.parse(await response1.text());
-  const available = body1.TipoTickets.filter(x => x.Disponible === 1);
-
-  // Notificar disponibles
-  if (!available.length) {
-    alert('No quedan tickets disponibles');
-    return;
-  }
-  alert(`Hay ${available.length} secciones disponibles`);
-
-  // Seleccionar ubicacion
-  let i = 0;
-  while (i < available.length) {
-    const seccion = available[i];
-    if (confirm(`Opción ${i + 1} de ${available.length} ¿Deseas comprar para la sección ${seccion.TipoTicket} por ${seccion.Precio}?`))
-      break;
-    i++;
-  }
-
-  // Verificar
-  if (i >= available.length) {
-    alert('No hay más secciones disponibles');
-    return;
-  }
-
-  // Obtener tipo
-  const jcTipoTicket = available[i].TipoTicketID;
-
-  // Confirmar cantidad
-  const jcCantidadTickets = confirm('¿Quieres dos entradas?') ? 2 : 1;
-
-  // Agregar al carrito
-  const response2 = await fetch("/Compra/AgregarMultipleTickets", {
-    headers: { "content-type": "application/json" },
-    body: JSON.stringify({ EventoID: JC_EVENTO, EventoCalendarioID: JC_NUM_FECHA, CategoriaTicketID: "1", Tickets: [{ TipoTicketID: jcTipoTicket, Cantidad: jcCantidadTickets }] }),
-    method: "POST",
-    credentials: "include"
-  });
-  const body2 = JSON.parse(await response2.text());
-  if (!body2.Success)
+  try
   {
-    alert('Error: ' + body2.ErrorList.join(', '));
-    return;
-  }
+    // Constantes
+    const JC_EVENTO = 'biz175';
+    const JC_NUM_FECHA = 1;
+    const JC_VERSION = 1;
 
-  alert ('Se agregaron las entradas al carro de compras. Redirigiendo al pago.');
-  window.location.href = '/Compra/Pago';
+    // Funciones UI
+    const mostrarMensaje = async (mensaje) =>
+    {
+      // FIXME: Cambiar
+      alert(mensaje);
+    };
+    const seleccionar = async (titulo, opciones, generarTexto) =>
+    {
+      // FIXME: Implementar correctamente
+      for (const opcion of opciones)
+      {
+        const texto = generarTexto(opcion);
+        if (confirm(`${titulo}: ${texto}`))
+          return opcion;
+      }
+
+      return undefined;
+    };
+
+    // Obtener ubicaciones disponibles
+    const response1 = await fetch("/Compra/TraerTipoTicketsSectores", {
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify({ eventoID: JC_EVENTO, eventoCalendarioID: JC_NUM_FECHA }),
+      method: "POST",
+      credentials: "include"
+    });
+    if (response1.url.includes('Account/SignIn'))
+    {
+      console.log(response1);
+      mostrarMensaje('Inicia sesión antes de empezar');
+      window.location.href = 'https://www.puntoticket.com/Account/SignIn';
+      return;
+    }
+    const body1 = JSON.parse(await response1.text());
+    const available = body1.TipoTickets.filter(x => x.Disponible === 1);
+
+    // Notificar disponibles
+    if (!available.length) {
+      await mostrarMensaje('No quedan tickets disponibles');
+      return;
+    }
+    await mostrarMensaje(`Hay ${available.length} secciones disponibles`);
+
+    // Seleccionar ubicacion
+    const selected = await seleccionar(
+      'Sección',
+      available,
+      seccion => `${seccion.TipoTicket} (${seccion.Precio})`);
+
+    // Verificar
+    if (selected === undefined) {
+      await mostrarMensaje('No hay más secciones disponibles');
+      return;
+    }
+
+    // Obtener tipo
+    const jcTipoTicket = selected.TipoTicketID;
+
+    // Confirmar cantidad
+    const jcCantidadTickets = await seleccionar('Cantidad de entradas', [1, 2], x => x.toString());
+    if (jcCantidadTickets === undefined)
+    {
+      await mostrarMensaje('No se ha seleecionado la cantidad de entradas');
+      return;
+    }
+
+    // Agregar al carrito
+    const response2 = await fetch("/Compra/AgregarMultipleTickets", {
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify({ EventoID: JC_EVENTO, EventoCalendarioID: JC_NUM_FECHA, CategoriaTicketID: "1", Tickets: [{ TipoTicketID: jcTipoTicket, Cantidad: jcCantidadTickets }] }),
+      method: "POST",
+      credentials: "include"
+    });
+    const body2 = JSON.parse(await response2.text());
+    if (!body2.Success)
+    {
+      await mostrarMensaje('Error: ' + body2.ErrorList.join(', '));
+      return;
+    }
+
+    await mostrarMensaje('Se agregaron las entradas al carro de compras. Redirigiendo al pago.');
+    window.location.href = '/Compra/Pago';
+  } catch (e) {
+    alert('Ocurrió un error: ' + e.message);
+  }
 })();
