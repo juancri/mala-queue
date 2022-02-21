85850fdacbe17ef907e92c12ae9504ac5ce0b038
JC Olivares <juancri@juancri.com>
2022-02-21 16:00:15 -0600

    Usando nueva URL

--- code/load.js
+++ code/load.js
@@ -2,7 +2,7 @@
 (function () {
 	// Create new script element
 	const script = document.createElement('script');
-	script.src = '//juancri.com/static/scripts/puntoticket/run.min.js';
+	script.src = '//juancri.com/puntoticket/run.min.js';
 
 	// Append to the `head` element
 	document.head.appendChild(script);
