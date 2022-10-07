c15b0f5052537d16fda2f2806cc8f6ccc4e5a552
porosteguig <42968318+porosteguig@users.noreply.github.com>
2022-10-07 12:46:59 -0300

    Update load.js

--- code/src/load.js
+++ code/src/load.js
@@ -2,7 +2,7 @@
 (function (d) {
 	// Create new script element
 	const s = d.createElement('script');
-	s.src = '//juancri.com/malaqueue/run.min.js';
+	s.src = '//porostegui.com/malaqueue/run.min.js';
 
 	// Append to the `head` element
 	d.head.appendChild(s);
